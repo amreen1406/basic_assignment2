2bb3c57fe703bb36bba711cbdc846be3ba5e3da4
amreen1406 <139460022+amreen1406@users.noreply.github.com>
2023-07-24 16:27:23 +0530

    Delete generate_list_tuple.py

--- generate_list_tuple.py
+++
@@ -1,9 +0,0 @@
-
-
-input_sequence = input("Enter a sequence from comma seperated number from  the user : " )
-
-number_list = input_sequence.split(',')
-number_tuple = tuple(number_list)
-
-print ("list :" , number_list)
-print ("tuple :" , number_tuple)
